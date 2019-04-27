b8ea224fa405ecc35b881687b0ab9e0a9438ec0a
wumengyu123 <w973985611@163.com>
2019-04-27 15:28:41 +0800

    two

---
+++ 3.字符串拼接.py
@@ -0,0 +1,18 @@
+#str '' 或者 ""
+'agdbd'
+"dkjfkdfd"
+"hello"
+"world"
+# +号
+print("hello" + "world")
+print("hello" , "world")
+# %
+a = "hello"
+b = "world"
+print("%s %s" % (a,b))
+
+#format
+print("{} {}".format(a,b))
+
+#f-string
+print(f"{a} {b}")
